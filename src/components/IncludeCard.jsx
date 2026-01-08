cb72c1a69d0e2b67d11c7678fc078d30e0782a14
MalakMM_Amer <malak.magdy888808@gmail.com>
2026-01-08 14:34:57 +0200

    Update IncludeCard.jsx

--- src/components/IncludeCard.jsx
+++ src/components/IncludeCard.jsx
@@ -4,7 +4,7 @@ function IncludeCard({ icon: Icon, title, description }) {
   return (
     <div className="flex justify-center">
       <div className="bg-white/10 bg-opacity-30 backdrop-blur-3xl p-12 rounded-xl text-white w-72 border border-white/60">
-        <div className="flex items-center gap-3 mb-3">
+        <div className="flex gap-3 mb-3">
           {Icon && <Icon className="text-2xl text-yellow-400" />}
           <h4 className="text-lg font-bold">{title}</h4>
         </div>
